16d4b70aade8d2d2abb5a83ab1d735e352779a94
Alan Wilner Moris Fermán <alan.wilner.moris@clases.edu.sv>
2026-07-05 22:21:55 -0600

    :rocket:feat: add Prisma setup and API key middleware
    - Created pnpm-workspace.yaml to allow builds for Prisma engines.
    - Added prisma.config.ts for Prisma configuration with environment variable for database URL.
    - Introduced schema.prisma for defining the database schema and generator settings.
    - Updated index.js to include users router and API key middleware.
    - Implemented apikey.middleware.js to validate API key from request headers.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "dotenv/config"
 import express from 'express'
+import usersRouter from './routes/users.routes.js'
+import {apikeyMiddleware} from './middleware/apikey.middleware.js'
 
 // CREAR INSTANCIA DE EXPRESS
 const app = express()
@@ -7,6 +9,13 @@ const PORT = process.env.PORT
 
 // ESPECIFICAR QUE TRABAJAREMOS CON JSON
 app.use(express.json())
+
+// MIDDLEWARE
+app.use(apikeyMiddleware)
+
+// ENDPOINTS
+app.use("/", usersRouter)
+
 
 // CREAMOS EL SERVER
 app.listen(PORT, () => {
